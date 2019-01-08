e0281b40e1c52c80d49a83754216016097f2f23d
Paul Gualotuna <paul.gualotuna@tum.de>
2019-01-08 17:51:50 +0100

    Use ngrx schematics to generate reducers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -9,37 +9,13 @@ import {environment} from '../../environments/environment';
 import {User} from '../model/user.model';
 import {AuthActionTypes} from '../auth/auth.actions';
 
-type AuthState = {
-  loggedIn: boolean,
-  user: User
-}
-
-const initialAuthState: AuthState = {
-  loggedIn: false,
-  user: undefined
-};
-
 export interface AppState {
-  auth: AuthState
-}
-
-function authReducer(state: AuthState = initialAuthState, action): AuthState {
-  switch (action.type) {
-    case AuthActionTypes.LoginAction:
-      return {
-        loggedIn: true,
-        user: action.payload.user
-      };
-    default:
-      return state;
-  }
 }
 
 export const reducers: ActionReducerMap<AppState> = {
 
-  auth: authReducer
-
 };
 
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] =
+  !environment.production ? [] : [];
